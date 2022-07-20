76f6aa616b25184f5217eb388c1aef2c99a940db
bashi8128 <itabasi.lm@gmail.com>
2022-07-20 13:14:52 +0900

    Change the output target from stdout to a file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use std::io::{self, BufRead ,BufReader, Write, BufWriter};
-use std::fs;
+use std::env;
+use std::io::{self, BufRead ,BufReader, Error, Write, BufWriter};
+use std::fs::{self, File};
 
 use chrono::{DateTime, Local};
 use termwiz::escape::{Action, ControlCode};
 use termwiz::escape::parser::Parser;
 
-fn main() {
+fn main() -> Result<(), Error>{
     let mut reader = BufReader::new(io::stdin());
 
     let mut read_buf = String::new();
     let mut parser = Parser::new();
 
-    let mut datetime: DateTime<Local> = Local::now();
-    let mut datetime_str: String = datetime.format("%F_%H%M%S%.f")
-                                                       .to_string();
+    let mut f: BufWriter<File>;
 
-    let mut f = BufWriter::new(fs::File::create(datetime_str + ".log")
-                                        .unwrap());
+    match create_logfile() {
+        Ok(file) => f = BufWriter::new(file),
+        Err(e) => return Err(e),
+    };
 
     while reader.read_line(&mut read_buf).unwrap() > 0 {
         let mut write_buf = String::new();
@@ -31,8 +32,8 @@ fn main() {
             _ => {}
         });
 
-        datetime = Local::now();
-        datetime_str = datetime.format("[%F %H%M%S%.f] ").to_string();
+        let datetime = Local::now();
+        let datetime_str = datetime.format("[%F %H%M%S%.f] ").to_string();
 
         write_buf = [datetime_str, write_buf].concat();
         match f.write(write_buf.as_bytes()) {
@@ -40,5 +41,27 @@ fn main() {
             Err(e) => eprintln!("Error in writing to file: {}", e),
         };
         read_buf = String::new();
-    }
+    };
+    Ok(())
+}
+
+fn create_logfile() -> Result<File, Error> {
+    let datetime: DateTime<Local> = Local::now();
+    let year: String = datetime.format("%Y").to_string();
+    let month: String = datetime.format("%m").to_string();
+    let date: String = datetime.format("%d").to_string();
+
+    let dirpath: String = env::var("HOME").unwrap() + "/log/" 
+                             + &year + "/"
+                             + &month + "/"
+                             + &date + "/";
+
+    match fs::create_dir_all(&dirpath) {
+        Ok(_) => {},
+        Err(e) => eprintln!("Error creating directory: {}", e),
+    };
+
+    let log_name: String = datetime.format(&(dirpath + &"%F_%H%M%S%.f.log"))
+                                   .to_string();
+    fs::File::create(log_name)
 }
