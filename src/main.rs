bb65c74ccef8d61e246b7ae6d7044ae4deba5b93
bashi8128 <itabasi.lm@gmail.com>
2022-08-07 17:43:51 +0900

    Add support for cursor move and some escape sequences
    Add support for below capabilities
    - Cursor Right/Left
    - EraseInDisplay
    - EraseInLine

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use chrono::{DateTime, Local};
 use termwiz::escape::{Action, ControlCode};
 use termwiz::escape::parser::Parser;
 
+use::termwiz::escape::csi::{CSI, Cursor, Edit, EraseInDisplay, EraseInLine};
+
 fn main() -> Result<(), Error>{
     let mut reader = BufReader::new(io::stdin());
 
-    let mut read_buf = String::new();
     let mut parser = Parser::new();
 
     let mut f: BufWriter<File>;
@@ -19,29 +20,30 @@ fn main() -> Result<(), Error>{
         Err(e) => return Err(e),
     };
 
+    let mut width: usize;
+    let mut read_buf = String::new();
     // Read a line from stdin and parse escape sequences in that line
-    while reader.read_line(&mut read_buf).unwrap() > 0 {
-        let mut write_buf = String::new();
-        parser.parse(&read_buf.as_bytes(), |action| match action {
-            Action::Print(c) => write_buf.push(c),
-            Action::Control(ctrl_code) => match ctrl_code {
-                ControlCode::Backspace => {write_buf.pop().unwrap();},
-                ControlCode::HorizontalTab => write_buf.push('\t'),
-                ControlCode::LineFeed => write_buf.push('\n'),
-                _ => {}
-            },
-            _ => {}
-        });
+    match reader.read_line(&mut read_buf) {
+        Ok(w) => width = w,
+        Err(e) => {
+            width = 0;
+            eprintln!("{}", e);
+        }
+    }
 
-        let datetime = Local::now();
-        let datetime_str = datetime.format("[%F %H:%M:%S.%f] ").to_string();
-
-        write_buf = [datetime_str, write_buf].concat();
-        match f.write(write_buf.as_bytes()) {
-            Ok(_) => {},
-            Err(e) => eprintln!("Error in writing to file: {}", e),
-        };
+    while width > 0 {
+        let actions = parser.parse_as_vec(&read_buf.as_bytes());
+        do_actions(actions, &mut f, width);
         read_buf = String::new();
+        match reader.read_line(&mut read_buf) {
+            Ok(w) => {
+                width = w;
+            },
+            Err(e) => {
+                width = 0;
+                eprintln!("{}", e);
+            }
+        }
     };
     Ok(())
 }
@@ -66,4 +68,95 @@ fn create_logfile() -> Result<File, Error> {
     let log_name: String = datetime.format(&(dirpath + &"%F_%H%M%S.%f.log"))
                                    .to_string();
     fs::File::create(log_name)
+}
+
+fn do_actions(actions: Vec<Action>, file: &mut BufWriter<File>, term_width: usize) {
+    let mut buffer = " ".repeat(term_width);
+    let mut cursor: usize = 0;
+    for action in actions.iter() {
+        match action {
+            Action::Print(c) => {
+                buffer.replace_range(cursor..cursor+1, &c.to_string());
+                cursor += 1;
+            },
+            Action::Control(ctrl_code) => match ctrl_code {
+                ControlCode::Backspace => {
+                    if cursor > 0 {
+                        cursor -= 1;
+                    };
+                    buffer.replace_range(cursor..cursor+1, " ");
+                },
+                ControlCode::HorizontalTab => {
+                    buffer.replace_range(cursor..cursor+1, "\t");
+                    cursor += 1;
+                },
+                _ => {}
+            },
+            Action::CSI(csi) => match csi {
+                CSI::Cursor(c) => match c {
+                    Cursor::Left(n) => {
+                        if cursor > 0 {
+                            cursor -= *n as usize;
+                        }
+                    },
+                    Cursor::Right(n) => {
+                        if cursor < term_width {
+                            cursor += *n as usize;
+                        }
+                    },
+                    _ => {}
+                },
+                CSI::Edit(e) => match e {
+                    Edit::EraseInDisplay(ed) => match ed {
+                        EraseInDisplay::EraseToEndOfDisplay => {
+                            for pos in cursor..term_width+1 {
+                                buffer.replace_range(pos..pos, " ");
+                            }
+                        },
+                        EraseInDisplay::EraseToStartOfDisplay => {
+                            for pos in 0..cursor+1 {
+                                buffer.replace_range(pos..pos, " ");
+                            }
+                            cursor = 0;
+                        },
+                        EraseInDisplay::EraseDisplay => {
+                            buffer = " ".repeat(term_width);
+                            cursor = 0;
+                        },
+                        EraseInDisplay::EraseScrollback => (),
+                    },
+                    Edit::EraseInLine(el) => match el {
+                        EraseInLine::EraseToEndOfLine => {
+                            for pos in cursor..term_width+1 {
+                                buffer.replace_range(pos..pos, " ");
+                            }
+                        },
+                        EraseInLine::EraseToStartOfLine => {
+                            for pos in 0..cursor+1 {
+                                buffer.replace_range(pos..pos, " ");
+                            }
+                            cursor = 0;
+                        },
+                        EraseInLine::EraseLine => {
+                            buffer = " ".repeat(term_width);
+                            cursor = 0;
+                        },
+                    }
+                    _ => {}
+                }
+                _ => {}
+            }
+            _ => {}
+       };
+    }
+    let datetime = Local::now();
+    let datetime_str = datetime.format("[%F %H:%M:%S.%f] ").to_string();
+
+    buffer = buffer.trim().to_string();
+    buffer = [datetime_str, buffer].concat();
+    buffer.push('\n');
+    match file.write(buffer.as_bytes()) {
+        Ok(_) => {},
+        Err(e) => eprintln!("Error in writing to file: {}", e),
+    };
 }
