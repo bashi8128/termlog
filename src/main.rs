f4376cb75713ffe63e1d299a5b71536b430b6e70
bashi8128 <itabasi.lm@gmail.com>
2022-08-13 17:57:44 +0900

    Refactor code using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::env;
-use std::io::{self, BufRead ,BufReader, Error, Write, BufWriter};
 use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, BufWriter, Error, Write};
 
 use chrono::{DateTime, Local};
-use termwiz::escape::{Action, ControlCode};
 use termwiz::escape::parser::Parser;
+use termwiz::escape::{Action, ControlCode};
 
-use::termwiz::escape::csi::{CSI, Cursor, Edit, EraseInDisplay, EraseInLine};
+use termwiz::escape::csi::{Cursor, Edit, EraseInDisplay, EraseInLine, CSI};
 
-fn main() -> Result<(), Error>{
+fn main() -> Result<(), Error> {
     let mut reader = BufReader::new(io::stdin());
 
     let mut parser = Parser::new();
@@ -38,13 +38,13 @@ fn main() -> Result<(), Error>{
         match reader.read_line(&mut read_buf) {
             Ok(w) => {
                 width = w;
-            },
+            }
             Err(e) => {
                 width = 0;
                 eprintln!("{}", e);
             }
         }
-    };
+    }
     Ok(())
 }
 
@@ -55,18 +55,17 @@ fn create_logfile() -> Result<File, Error> {
     let month: String = datetime.format("%m").to_string();
     let date: String = datetime.format("%d").to_string();
 
-    let dirpath: String = env::var("HOME").unwrap() + "/log/" 
-                             + &year + "/"
-                             + &month + "/"
-                             + &date + "/";
+    let dirpath: String =
+        env::var("HOME").unwrap() + "/log/" + &year + "/" + &month + "/" + &date + "/";
 
     match fs::create_dir_all(&dirpath) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => eprintln!("Error creating directory: {}", e),
     };
 
-    let log_name: String = datetime.format(&(dirpath + &"%F_%H%M%S.%f.log"))
-                                   .to_string();
+    let log_name: String = datetime
+        .format(&(dirpath + &"%F_%H%M%S.%f.log"))
+        .to_string();
     fs::File::create(log_name)
 }
 
@@ -78,42 +77,42 @@ fn do_actions(actions: Vec<Action>, file: &mut BufWriter<File>, term_width: usiz
         match action {
             // Print a single character to the buffer by replacing it with a space
             Action::Print(c) => {
-                buffer.replace_range(cursor..cursor+1, &c.to_string());
+                buffer.replace_range(cursor..cursor + 1, &c.to_string());
                 cursor += 1;
-            },
+            }
             Action::Control(ctrl_code) => match ctrl_code {
                 // Move cursor one columns left and replace a character at the cursor with a space
                 ControlCode::Backspace => {
                     if cursor > 0 {
                         cursor -= 1;
                     };
-                    buffer.replace_range(cursor..cursor+1, " ");
-                },
+                    buffer.replace_range(cursor..cursor + 1, " ");
+                }
                 // Replace a space with a horizontal tab
                 ControlCode::HorizontalTab => {
-                    buffer.replace_range(cursor..cursor+1, "\t");
+                    buffer.replace_range(cursor..cursor + 1, "\t");
                     cursor += 1;
-                },
+                }
                 // Move cursor to the start of the line
                 ControlCode::CarriageReturn => {
                     cursor = 0;
-                },
+                }
                 _ => {}
             },
             Action::CSI(csi) => match csi {
                 CSI::Cursor(c) => match c {
-                // Move cursor left n columns
+                    // Move cursor left n columns
                     Cursor::Left(n) => {
                         if cursor > 0 {
                             cursor -= *n as usize;
                         }
-                    },
-                // Move cursor right n columns
+                    }
+                    // Move cursor right n columns
                     Cursor::Right(n) => {
                         if cursor < term_width {
                             cursor += *n as usize;
                         }
-                    },
+                    }
                     _ => {}
                 },
                 CSI::Edit(e) => match e {
@@ -121,53 +120,53 @@ fn do_actions(actions: Vec<Action>, file: &mut BufWriter<File>, term_width: usiz
                         // Replace characters from the cursor position
                         // to the end of the line with spaces
                         EraseInDisplay::EraseToEndOfDisplay => {
-                            for pos in cursor..term_width+1 {
+                            for pos in cursor..term_width + 1 {
                                 buffer.replace_range(pos..pos, " ");
                             }
-                        },
+                        }
                         // Replace characters from the start of the line
                         // to the current cursor position with spaces
                         EraseInDisplay::EraseToStartOfDisplay => {
-                            for pos in 0..cursor+1 {
+                            for pos in 0..cursor + 1 {
                                 buffer.replace_range(pos..pos, " ");
                             }
                             cursor = 0;
-                        },
+                        }
                         // Re-create the write buffer
                         EraseInDisplay::EraseDisplay => {
                             buffer = " ".repeat(term_width);
                             cursor = 0;
-                        },
+                        }
                         EraseInDisplay::EraseScrollback => (),
                     },
                     Edit::EraseInLine(el) => match el {
                         // Replace characters from the cursor position
                         // to the end of the line with spaces
                         EraseInLine::EraseToEndOfLine => {
-                            for pos in cursor..term_width+1 {
+                            for pos in cursor..term_width + 1 {
                                 buffer.replace_range(pos..pos, " ");
                             }
-                        },
+                        }
                         // Replace characters from the start of the line
                         // to the current cursor position with spaces
                         EraseInLine::EraseToStartOfLine => {
-                            for pos in 0..cursor+1 {
+                            for pos in 0..cursor + 1 {
                                 buffer.replace_range(pos..pos, " ");
                             }
                             cursor = 0;
-                        },
+                        }
                         // Re-create the write buffer
                         EraseInLine::EraseLine => {
                             buffer = " ".repeat(term_width);
                             cursor = 0;
-                        },
-                    }
+                        }
+                    },
                     _ => {}
-                }
+                },
                 _ => {}
-            }
+            },
             _ => {}
-       };
+        };
     }
     let datetime = Local::now();
     let datetime_str = datetime.format("[%F %H:%M:%S.%f] ").to_string();
@@ -176,7 +175,7 @@ fn do_actions(actions: Vec<Action>, file: &mut BufWriter<File>, term_width: usiz
     buffer = [datetime_str, buffer].concat();
     buffer.push('\n');
     match file.write(buffer.as_bytes()) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(e) => eprintln!("Error in writing to file: {}", e),
     };
 }
